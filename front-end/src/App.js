2e5e22d24982a37842bdec759d8fcd0e474da185
Neil Chadwick <neilchadders@hotmail.com>
2024-05-04 09:44:54 +0100

    add HomeScreen and ProductScreen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,21 @@
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-  Hi
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+     
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
