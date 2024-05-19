35a4b4b6ffb9f08277c84e2898ff0096748321eb
Neil Chadwick <neilchadders@hotmail.com>
2024-05-19 09:54:13 +0100

    update orderController - paypal functioning

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,14 @@
+import path from 'path'; //join and resolve methods
 import express from 'express';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; //automatically loads environment variables from a .env
 import cookieParser from 'cookie-parser';
-dotenv.config();
+dotenv.config(); //requires and config dotenv
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js'
-import {notFound, errorHandler } from "./middleware/errorMiddleware.js"
-
-
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
@@ -16,27 +16,39 @@ connectDB();
 
 const app = express();
 
-//Body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //For post and put req - parses incoming JSON requests and puts the parsed data in req.body.
+app.use(express.urlencoded({ extended: true })); // recognises the incoming Request Object as strings or arrays
+app.use(cookieParser()); //parses incoming cokies
 
-//cookie parser
-app.use(cookieParser())
-
-app.get('/', (req, res) => {
-    res.send('Api is running...!');
-});
-
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/front-end/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html'))
   );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
 
-app.listen(port, () => console.log(`Server running on ${port}`))
