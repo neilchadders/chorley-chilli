b9922f3ec02fca59ca7edeb55d59388e2e17a9ac
Neil Chadwick <neilchadders@hotmail.com>
2024-10-05 11:42:05 +0100

    make carousel same size for each img

--- front-end/src/screens/IntroScreen.jsx
+++ front-end/src/screens/IntroScreen.jsx
@@ -1,19 +1,18 @@
 
-import logo from '../background2.jpg';
 import './intro.css'
+import ProductCarousel from '../components/ProductCarousel';
 
 const IntroScreen = () => {
     return (
      
         <div className='centre'>
             <p  className='intro_text'>J F Bell's</p>
-            <p className='intro_text'>Chorley Chilli Sauces</p>
+            <p className='intro_text'>Chorley Chilli Sauces</p>     
        
-       
-        <div className='intro_img'>
-                <img src= {logo} alt="chilli-pic" />
+        <div className='intro_text_carousel'>
+             <ProductCarousel/>
         </div>
-
+       
         </div>
       
     );
