010b7e760ef62cd89dbe9f5801236144375b3b07
Neil Chadwick <neilchadders@hotmail.com>
2024-11-10 18:41:57 +0000

    create universal background css class

--- front-end/src/components/CheckoutSteps.jsx
+++ front-end/src/components/CheckoutSteps.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import 'formcontainer.css';
+import './formcontainer.css';
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
