e4e7df98f2cafe3d5c43bbf7e24144f5b3cfbbe3
Neil Chadwick <neilchadders@hotmail.com>
2024-11-11 19:39:10 +0000

    create universal background css class

--- front-end/src/screens/HomeScreen.jsx
+++ front-end/src/screens/HomeScreen.jsx
@@ -32,7 +32,7 @@ const HomeScreen = () => {
       ) : (
         <>
           <Meta id = "homescreen" className = "screen-background"/>
-          <h1>Latest Products</h1>
+          <h1 className = "screen-background">Latest Products</h1>
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
