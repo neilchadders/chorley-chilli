10317a2c51a12274cfe2eb6fa71fd4b2f72912b0
Neil Chadwick <neilchadders@hotmail.com>
2024-11-25 11:35:15 +0000

    amend forge-password link

--- front-end/src/screens/ForgetPasswordScreen.jsx
+++ front-end/src/screens/ForgetPasswordScreen.jsx
@@ -1,35 +1,34 @@
+import React, { useState } from 'react';
 import FormContainer from "../components/FormContainer";
-import { Form, Button, } from 'react-bootstrap';
-
-const [email, setEmail] = "";
-
-const submitForgotEmail = (e) => {
-  e.preventDefault();
-  console.log(email);
-};
+import { Form, Button } from 'react-bootstrap';
 
 const ForgetPasswordScreen = () => {
+  const [email, setEmail] = useState(""); // Correct state initialization
+
+  const submitForgotEmail = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
   return (
     <div>
       <h1>Forget Password</h1>
       <FormContainer>
-      <Form onSubmit={submitForgotEmail}>
-      <Form.Group className='my-2' controlId='email'>
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Enter email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        
-        <Button type='submit' variant='primary'>
-          Reset Password
-        </Button>
-    </Form>
+        <Form onSubmit={submitForgotEmail}>
+          <Form.Group className='my-2' controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} // Update state correctly
+            ></Form.Control>
+          </Form.Group>
+          <Button type='submit' variant='primary'>
+            Reset Password
+          </Button>
+        </Form>
       </FormContainer>
-
     </div>
   );
 };
