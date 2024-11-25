b2141ff65f582c6e650618fbed77f9d637e8c62e
Neil Chadwick <neilchadders@hotmail.com>
2024-11-25 10:43:37 +0000

    amend forge-password link

--- front-end/src/screens/ForgetPasswordScreen.jsx
+++ front-end/src/screens/ForgetPasswordScreen.jsx
@@ -1,9 +1,30 @@
+import FormContainer from "../components/FormContainer";
+import { Form, Button, } from 'react-bootstrap';
 
+const [email, setEmail] = "";
 
 const ForgetPasswordScreen = () => {
   return (
     <div>
       <h1>Forget Password</h1>
+      <FormContainer>
+
+      <Form.Group className='my-2' controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        
+        <Button type='submit' variant='primary'>
+          Reset Password
+        </Button>
+
+      </FormContainer>
+
     </div>
   );
 };
