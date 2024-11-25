645a959b1ee70f42ca4bd3796e89a306bc0bd57e
Neil Chadwick <neilchadders@hotmail.com>
2024-11-25 18:16:54 +0000

    add reset password function

--- back-end/controllers/forgetPasswordController.js
+++ back-end/controllers/forgetPasswordController.js
@@ -1,5 +1,5 @@
 import crypto from "crypto";
-import User from "../models/User.js";
+import User from '../models/userModel.js';
 import sendEmail from "../utils/sendEmail.js"; // Create this utility
 
 // Request password reset
