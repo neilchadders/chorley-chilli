825501dd0ef171904b8116b887ec104896e003b5
Neil Chadwick <neilchadders@hotmail.com>
2024-11-27 08:37:44 +0000

    style contact form

--- front-end/src/constants.js
+++ front-end/src/constants.js
@@ -1,7 +1,7 @@
 
-//export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '';
 
-export const BASE_URL = ''; //If using proxy-use when running locally
+//export const BASE_URL = ''; //If using proxy-use when running locally
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
