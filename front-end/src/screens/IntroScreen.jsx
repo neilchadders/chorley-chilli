5aa8a084ec3b338f6fe69fd5133cb86c5fdd5363
Neil Chadwick <neilchadders@hotmail.com>
2024-12-09 09:11:58 +0000

    amend font styling

--- front-end/src/screens/IntroScreen.jsx
+++ front-end/src/screens/IntroScreen.jsx
@@ -1,23 +1,25 @@
-
 import './intro.css'
 import './screen.background.css'
+import ProductCarousel from '../components/ProductCarousel'; // Import the ProductCarousel component
 
 const IntroScreen = () => {
     return (
-     
+
         <div className = "screen-background">
             <div id = "title-text">
             <p className='title-head'>J. F. Bell</p> 
             <p  className='intro-text'>Chorley Chilli Sauces</p>
             <p className='intro-text'>Tasty Spices at Tasty Prices</p>
             </div>
-                 
-       
-       
-       
+
+
+        <div className='intro_text_carousel'>
+             <ProductCarousel/> 
         </div>
-      
+
+        </div>
+
     );
 }
 
-export default IntroScreen; 
+export default IntroScreen;
