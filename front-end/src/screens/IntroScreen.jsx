68966c00ab895dd9c6d36039f08db1a05f4977ea
Neil Chadwick <neilchadders@hotmail.com>
2024-12-10 09:01:04 +0000

    add background image and change header colour

--- front-end/src/screens/IntroScreen.jsx
+++ front-end/src/screens/IntroScreen.jsx
@@ -2,12 +2,13 @@ import './intro.css'
 import './screen.background.css'
 import ProductCarousel from '../components/ProductCarousel'; // Import the ProductCarousel component
 import { ReactTyped } from "react-typed";
-
+import habanero from './habanerowhiteback.jpg'
 
 const IntroScreen = () => {
     return (
 
         <div className = "screen-background">
+            
             <div id = "title-text">
             <p className='title-head'>J. F. Bell</p> 
             <p  className='intro-text'>Chorley Chilli Sauces</p>
